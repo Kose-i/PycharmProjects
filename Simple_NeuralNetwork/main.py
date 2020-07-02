d842a6da1460a73e91b95e39425b9e3d22514112
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-07-02 17:01:12 +0900

    Modify Simple_NeuralNet-main

--- Simple_NeuralNetwork/main.py
+++ Simple_NeuralNetwork/main.py
@@ -8,41 +8,33 @@ from keras.utils import np_utils
 
 if __name__=='__main__':
     (x_train, t_train), (x_test, t_test) = load_data() # mnist-data
-    train_loss_list = []
-    t_train = np_utils.to_categorical(t_train, 10)
-    t_test  = np_utils.to_categorical(t_test,  10)
+    t_train = np_utils.to_categorical(t_train, 10).astype('float32')
+    t_test  = np_utils.to_categorical(t_test,  10).astype('float32')
+    x_train = x_train.reshape(x_train.shape[0], 784)
+    x_test = x_test.reshape(x_test.shape[0], 784)
+    train_size = x_train.shape[0]
 
+    train_loss_list = []
+    train_acc_list = []
+    test_acc_list = []
     # hyper parameters
     iters_num = 10000
-    #iters_num = 1
-    print(x_train.shape)
-    train_size = x_train.shape[0]
-    x_train_reshape = x_train.reshape(train_size, 784)
     batch_size = 100
     learning_rate = 0.1
 
     network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
-    #print("accuracy:", network.accuracy(x_train_reshape, t_train))
+
     for i in range(iters_num):
         batch_mask = np.random.choice(train_size, batch_size)
-        x_batch = x_train_reshape[batch_mask].astype('float32') / 255.0
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
 
-        t_batch = t_train[batch_mask].astype('float32')
-        #print(x_batch.shape)
-        #print(t_batch.shape)
-
-        #grad = network.numerical_gradient(x_batch, t_batch)
-        grad = network.gradient(x_batch, t_batch)
-        #print(grad)
+        grad = network.numerical_gradient(x_batch, t_batch)
+        #grad = network.gradient(x_batch, t_batch)
         for key in ('W1', 'b1', 'W2', 'b2'):
             network.params[key] -= learning_rate * grad[key]
         loss = network.loss(x_batch, t_batch)
-        print(loss)
-        if len(train_loss_list) > 10 and train_loss_list[-1]<loss:
-            #print(train_loss_list)
-            #print("accuracy:", network.accuracy(x_test_reshape, t_test))
-            exit()
         train_loss_list.append(loss)
-
+    print(train_loss_list)
 
     #print("accuracy:", network.accuracy(x_test_reshape, t_test))
